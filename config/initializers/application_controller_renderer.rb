a13a0c9d26241aebca8bc989e4f36eb28e808d22
maeshu-23 <maeshu619@gmail.com>
2020-08-23 22:17:46 +0900

    add edit and update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,8 @@
 #     https: false
 #   )
 # end
+
+
+
+
+Refile.secret_key = '43cd7ca77ceeafceafece1ea8b106f04e85b879c7b2db35b1d9afacc054ca371f2fc2601e5d971bdd1c04842fa795e6385753f330ec083da360bc719be875be9'
